933a036579ca32a4f53bcbb5e29106cb81725224
mgl <1375022007@qq.com>
2020-09-06 11:39:14 +0800

    and some webpack config

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import React, { PureComponent } from 'react'
 import reactDOM from 'react-dom'
+import 'font-awesome/css/font-awesome.min.css'
 import './index.css'
+import './test.scss'
+import breadImg from './assets/images/bread.jpg'
 
 const App=()=>{
   return (
-    <div>hello world react</div>
+    <div>
+      hello world react and you learning
+      <i className="fa fa-address-book" aria-hidden="true"></i>
+      {/* <img src={breadImg} /> */}
+    </div>
+    
   )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path=require('path')
-const HtmlWebpackPlugin=require('html-webpack-plugin')
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const {CleanWebpackPlugin}=require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports={
   mode:'development',
   entry:'./src/app.js',
   output:{
     path:path.resolve(__dirname,'dist'),
-    filename:'app.js'
+    filename:'js/app.js'
+  },
+  optimization:{
+    splitChunks:{
+      chunks:'all'
+    }
+  },
+  devtool:'inline-source-map',
+  devServer:{
+    contentBase:'./dist'
   },
   module: {
     rules: [
@@ -22,15 +33,44 @@ module.exports={
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test:/\.s[ac]ss$/i,
+        use:['style-loader','css-loader','sass-loader']
+      },
+      {
+        test:/\.(png|jpg|gif)$/i,
+        use:[{
+          loader:'url-loader',
+          options:{
+            limit:8192,
+            name:'assets/image/[name].[ext]'
+          }
+        }]
+      },
+      {
+        test:/\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use:[{
+          loader:'url-loader',
+          options:{
+            limit:8192,
+            name:'assets/font/[name].[ext]'
+          }
+        }]
+      }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin(
       {
         template:'./src/index.html'
       }
-    )
+    ),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/[name].css',
+      chunkFilename: '[id].css',
+    })
   ]
 }
